4a16ffd0d1d96507de1a0a36e5acf72f92c2da8a
DruAshley <druashley@gmail.com>
2020-02-22 08:55:04 -0800

    set up the base grid layout for the todo list.

--- src/addButton.js
+++ src/addButton.js
@@ -5,15 +5,35 @@ import { testTask } from "./todoObject.js"
 
 function setNewTask () {
     const newTask = document.createElement('div');
-    newTask.innerHTML = `The title of this task is: ${testTask.title}`
+    const taskTitle = document.createElement('div');
+    const taskDescription = document.createElement('div');
+    const taskDueDate = document.createElement('div');
+    const taskPriority = document.createElement('div');
+    const taskNotes = document.createElement('div')
+   
+    taskTitle.textContent = testTask.title;
+    taskDescription.textContent = testTask.description;
+    taskDueDate.textContent = testTask.dueDate;
+    taskPriority.textContent = testTask.priority;
+    taskNotes.textContent = testTask.notes;
 
-    container.appendChild(newTask)
+
+    // newTask.innerHTML = `${testTask.title} due date is ${testTask.dueDate} and has the following notes: ${testTask.notes}.`
+    newTask.classList.add("task");
+
+
+    newTask.appendChild(taskTitle);
+    newTask.appendChild(taskDescription);
+    newTask.appendChild(taskDueDate);
+    newTask.appendChild(taskPriority);
+    newTask.appendChild(taskNotes);
+
+    container.appendChild(newTask) 
+//    container.appendChild(testTask.testMethod())
 }
 
 const setAddButton = () => {
-    addButton.addEventListener("click", function(){
-        container.appendChild(testTask.testMethod())
-    })
+    addButton.addEventListener("click", setNewTask)
 }
 
 export {setAddButton}
--- src/todoObject.js
+++ src/todoObject.js
@@ -13,13 +13,7 @@ const todoTask = ({ title, description, dueDate, priority, notes }) => ({
     testMethod: () => {
      return `The task titled ${title} has the following notes: ${notes}`
     },
-    testMaker: () => {
-        const newTask = document.createElement('div');
-        newTask.textContent = `${title} due date is ${dueDate} and has the following notes: ${notes}.`;
-        return newTask
-    }
-
-})
+});
 
 
 

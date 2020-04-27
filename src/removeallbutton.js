5f21a7047eaf91788a134464e02b56c23b890a65
Druashley <druashley@gmail.com>
2020-04-27 12:48:28 -0700

    made the remove checked tasks button

--- src/removeallbutton.js
+++ src/removeallbutton.js
@@ -1,7 +1,14 @@
 import { container, createButton, completeButton, deleteChecked } from "./domelements.js"
 
-function removeChecked () {
-    console.log('test');
+
+// this js page is for the function of the button at the bottom of the to do page
+function removeChecked() {
+    //console.log(container.childElementCount)
+    for (let i = 0; i < container.childElementCount; i++) {
+        if (container.children[i].firstElementChild.textContent == "●"){
+            container.children[i].remove()
+        }
+    };
 }
 
 const removeButton = () => {
